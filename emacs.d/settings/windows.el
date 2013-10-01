dd1833a875c1ba0f2ea405e100bd136b9c575075
Nick Henderson <nwh@stanford.edu>
2013-10-01 10:41:09 -0700

    update emacs windmove for gnome 3

--- emacs.d/settings/windows.el
+++ emacs.d/settings/windows.el
@@ -2,8 +2,12 @@
 
 ;; this allows me to use super-arrow to move between windows.
 ;; very useful and does not mess up orgmode
-(windmove-default-keybindings 'super)
-;(windmove-default-keybindings 'meta)
+;;(windmove-default-keybindings 'super)
+;;(windmove-default-keybindings 'meta)
+(global-set-key (kbd "<M-s-up>")     'windmove-up)
+(global-set-key (kbd "<M-s-down>")   'windmove-down)
+(global-set-key (kbd "<M-s-left>")   'windmove-left)
+(global-set-key (kbd "<M-s-right>")  'windmove-right)
 
 ;; keys to easily resize windows
 (global-set-key (kbd "C-s-<left>") 'shrink-window-horizontally)
