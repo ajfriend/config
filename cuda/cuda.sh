d3a3e8ff04dac3cd0e4d18c932b1e053d1745440
Nick Henderson <nwh@stanford.edu>
2013-10-07 16:48:29 -0700

    added cuda configuration files

---
+++ cuda/cuda.sh
@@ -0,0 +1,3 @@
+# file: /etc/profile.d/cuda.sh
+# add cuda 5.5 to the path
+export PATH=/usr/local/cuda-5.5/bin:${PATH}
---
+++ cuda/cuda_setup.sh
@@ -0,0 +1,4 @@
+#/usr/bin/bash
+
+sudo cp cuda.sh /etc/profile.d/cuda.sh
+sudo cp cuda.conf /ect/ld.so.conf.d/cuda.conf
